d0a07ae4282db49962293f328deffcd1855b239f
Emmanuel Villalobos <villalobos.emmanuel@gmail.com>
2024-04-03 20:29:00 -0600

    Initial commit

---
+++ src/main/java/dev/emmanuelva/javahtmxhospital/JavaHtmxHospitalApplication.java
@@ -0,0 +1,13 @@
+package dev.emmanuelva.javahtmxhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHtmxHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHtmxHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/emmanuelva/javahtmxhospital/JavaHtmxHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emmanuelva.javahtmxhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHtmxHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
